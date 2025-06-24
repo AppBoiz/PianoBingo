44869d96ef5b4524f37d28f8fa09a6d416cde08b
winflag10 <53182120+winflag10@users.noreply.github.com>
2025-06-24 20:15:36 +0100

    Game history page done

--- services/navigation/navigation.js
+++ services/navigation/navigation.js
@@ -1,7 +1,8 @@
 const PAGE = {
   GAME: 'pages/pdf-reader/pdf-reader.html',
   WELCOME: 'pages/welcome-page/welcome-page.html',
-  PACK_SELECT: 'pages/pack-select/pack-select.html'
+  PACK_SELECT: 'pages/pack-select/pack-select.html',
+  GAME_HISTORY: 'pages/game-history/game-history.html'
 };
 
 let last_page = "";
