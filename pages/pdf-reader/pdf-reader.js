fd1a75532dd6d1b68b895ec60d65b49d6131f5c3
winflag10 <53182120+winflag10@users.noreply.github.com>
2025-06-25 22:11:13 +0100

    Some style formatting on game page

--- pages/pdf-reader/pdf-reader.js
+++ pages/pdf-reader/pdf-reader.js
@@ -1,40 +1,44 @@
-// Ensure the pdfBase64 variable is available after the pdf-base64.js is imported
-if (typeof pdfBase64 === 'undefined') {
-    console.error('pdfBase64 is not defined. Make sure pdf-base64.js is properly loaded.');
-} else {
-    const container = document.getElementById('pdf-viewer');
-
-    // Decode the Base64 string and load the PDF as a binary string
-    const byteArray = new Uint8Array(atob(pdfBase64).split("").map(function(c) { return c.charCodeAt(0); }));
-    const pdfData = new Uint8Array(byteArray);
-
-    pdfjsLib.GlobalWorkerOptions.workerSrc =
+pdfjsLib.GlobalWorkerOptions.workerSrc =
     'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
-    pdfjsLib.getDocument({ data: pdfData }).promise.then(pdf => {
-    const renderPage = pageNum => {
-        return pdf.getPage(pageNum).then(page => {
-        const viewport = page.getViewport({ scale: 1 });
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+function load(){
+    // Ensure the pdfBase64 variable is available after the pdf-base64.js is imported
+    if (typeof pdfBase64 === 'undefined') {
+        console.error('pdfBase64 is not defined. Make sure pdf-base64.js is properly loaded.');
+    } else {
+        const container = document.getElementById('pdf-viewer');
 
-        container.appendChild(canvas);
+        // Decode the Base64 string and load the PDF as a binary string
+        const byteArray = new Uint8Array(atob(pdfBase64).split("").map(function(c) { return c.charCodeAt(0); }));
+        const pdfData = new Uint8Array(byteArray);
 
-        return page.render({
-            canvasContext: context,
-            viewport: viewport
-        }).promise;
+        pdfjsLib.getDocument({ data: pdfData }).promise.then(pdf => {
+        const renderPage = pageNum => {
+            return pdf.getPage(pageNum).then(page => {
+            const c_container = document.getElementById("pdf-viewer")
+            const viewport = page.getViewport({scale:c_container.clientHeight / page.getViewport({scale:1}).height});
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            container.appendChild(canvas);
+
+            return page.render({
+                canvasContext: context,
+                viewport: viewport
+            }).promise;
+            });
+        };
+
+        const renderAllPages = async () => {
+            for (let i = 1; i <= pdf.numPages; i++) {
+            await renderPage(i);
+            }
+        };
+
+        renderPage(1)
+        // renderAllPages();
         });
-    };
-
-    const renderAllPages = async () => {
-        for (let i = 1; i <= pdf.numPages; i++) {
-        await renderPage(i);
-        }
-    };
-
-    renderAllPages();
-    });
+    }
 }
