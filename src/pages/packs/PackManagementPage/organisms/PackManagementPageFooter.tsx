a628158a93773d7e2b12c6621ce66741e63d615e
James Pedersen <james.richard.pedersen@gmail.com>
2026-03-21 19:19:32 +0100

    changed create new button to pink

--- src/pages/packs/PackManagementPage/organisms/PackManagementPageFooter.tsx
+++ src/pages/packs/PackManagementPage/organisms/PackManagementPageFooter.tsx
@@ -6,7 +6,7 @@ export default function PackManagementPageFooter({ onCreateNewPack }: PackManage
   return (
     <div className="flex w-full items-center justify-center px-4 py-4">
       <button
-        className="create-button rounded-lg bg-fuchsia-600 px-5 py-2.5 text-base font-semibold text-white shadow-md transition hover:bg-fuchsia-700"
+        className="create-button rounded-lg bg-brand-pink px-5 py-2.5 text-base font-semibold text-white shadow-md transition hover:bg-brand-pinkDark"
         data-action="create-pack"
         onClick={onCreateNewPack}
       >
